3ccd735b81e2023f52d6d167f1f85c07a384edd3
Ilya Ponetayev <i.ponetaev@ndmsystems.com>
2017-10-24 13:33:27 +0300

    socket.c: try not to use previous address

--- src/openvpn/socket.c
+++ src/openvpn/socket.c
@@ -1798,6 +1798,7 @@ resolve_remote(struct link_socket *sock,
         }
     }
 
+#if !defined(ENABLE_NDM_INTEGRATION)
     /* should we re-use previous active remote address? */
     if (link_socket_actual_defined(&sock->info.lsa->actual))
     {
@@ -1809,6 +1810,7 @@ resolve_remote(struct link_socket *sock,
         }
     }
     else
+#endif /* !defined(ENABLE_NDM_INTEGRATION) */
     {
         CLEAR(sock->info.lsa->actual);
         if (sock->info.lsa->current_remote)
